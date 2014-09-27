a2be0604db36a8839715fe21121d64473246919c
Shamiq <i.shamiq@gmail.com>
2014-09-27 12:40:58 -0700

    Initial Commit

---
+++ The Best Chocolate Chip CookiesTests/The_Best_Chocolate_Chip_CookiesTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Best_Chocolate_Chip_CookiesTests.swift
+//  The Best Chocolate Chip CookiesTests
+//
+//  Created by Shamiq Islam on 9/27/14.
+//  Copyright (c) 2014 MoogleCookers. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class The_Best_Chocolate_Chip_CookiesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
